74eadbfcd6b837649b8869b950961056764e75e9
Drew Baumann <drewbaumann@gmail.com>
2013-10-10 05:26:51 +0800

    removed a bad end.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,13 @@
 class MessagesController < ApplicationController
   def retrieve
-    full_phone = IsoCountryCodes.find(params[:FromCountry]).calling + params[:From]
+    country_code = IsoCountryCodes.find(params[:FromCountry]).calling
+    full_phone = "#{country_code}#{params[:From]}"
     user = User.find_by_phone_number(full_phone)
     body = params[:Body]
     user.respond_to_incoming_message(body)
 
     
     render status :status
-    end
   end
 
 end
