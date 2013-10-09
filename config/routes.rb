157cbe0085cfe08dd3d697858343c1ab0da9a2e1
Drew Baumann <drewbaumann@gmail.com>
2013-10-09 21:29:19 +0800

    created routes for the end points.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Bonk::Application.routes.draw do
+  resources :users
+
+  match '/messages/retrieve' => 'messages#retrieve', :via => [:POST]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
