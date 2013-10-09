6ddcc2cb064798f5232867bde32d6c32c8856386
Drew Baumann <drewbaumann@gmail.com>
2013-10-09 14:09:48 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bonk::Application.config.secret_token = 'e0236a5052cad0aa5958cd72a0448feb1fdd0eab8b5545fa814367784cb5ffbcd406e4d63f7ee9fb93e52a25790eefea072fca3a10385e974e56c06318ca5528'
