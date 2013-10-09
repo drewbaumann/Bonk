33e28a8f93f4a2abfd6ae703be9119d8a4c8d05c
Drew Baumann <drewbaumann@gmail.com>
2013-10-09 21:29:36 +0800

    created messages controller to handle texts.

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,13 @@
+class MessagesController < ApplicationController
+  include ActionView::Helpers::DateHelper
+  def retrieve
+    user = User.find_by_phone_number(params[:From])
+    body = params[:Body]
+    user.respond_to_incoming_message(body)
+
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+end
